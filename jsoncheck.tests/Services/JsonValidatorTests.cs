ace33fb3a2f15b9bdc38d5ceabafcfea97fc263c
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2026-01-15 21:01:14 +0100

    Added more test to cover more of the strict behaviour in the validation

--- jsoncheck.tests/Services/JsonValidatorTests.cs
+++ jsoncheck.tests/Services/JsonValidatorTests.cs
@@ -14,7 +14,7 @@ public class JsonValidatorTests
     }
 
     [Fact]
-    public void Validate_EmptyInput_RetursnEmptyInputError()
+    public void Validate_EmptyInput_ReturnsEmptyInputError()
     {
         var validator = new JsonValidator();
         var result = validator.Validate("   ");
@@ -34,7 +34,7 @@ public class JsonValidatorTests
     }
 
     [Fact]
-    public void Validate_SyntadError_ReturnsSyntaxErrorWithLocation()
+    public void Validate_SyntaxError_ReturnsSyntaxErrorWithLocation()
     {
         var validator = new JsonValidator();
         var result = validator.Validate("{\"a\":}");
@@ -44,5 +44,103 @@ public class JsonValidatorTests
         Assert.NotNull(result.ErrorMessage);
         Assert.NotNull(result.LineNumber);
         Assert.NotNull(result.LinePosition);
+    }
+
+    [Fact]
+    public void Validate_UnquotedPropertyAndTrailingComma_IsInvalid()
+    {
+        var validator = new JsonValidator();
+        var input = "{ Nummer: 1234, }";
+
+        var result = validator.Validate(input);
+
+        Assert.False(result.IsValid);
+        Assert.Equal(JsonErrorKind.SyntaxError, result.ErrorKind);
+    }
+
+    [Fact]
+    public void Validate_TrailingCommaInArray_IsInvalid()
+    {
+        var validator = new JsonValidator();
+        var input = "[1, 2,]";
+
+        var result = validator.Validate(input);
+
+        Assert.False(result.IsValid);
+        Assert.Equal(JsonErrorKind.SyntaxError, result.ErrorKind);
+    }
+
+    [Fact]
+    public void Validate_JsonWithComments_IsInvalid()
+    {
+        var validator = new JsonValidator();
+        var input = "{ \"a\": 1 /* comment */ }";
+
+        var result = validator.Validate(input);
+
+        Assert.False(result.IsValid);
+        Assert.Equal(JsonErrorKind.SyntaxError, result.ErrorKind);
+    }
+
+    [Fact]
+    public void Validate_ValidJsonArray_ReturnsValid()
+    {
+        var validator = new JsonValidator();
+        var result = validator.Validate("[1,2,3]");
+
+        Assert.True(result.IsValid);
+    }
+
+    [Fact]
+    public void Validate_WhitespaceAroundJson_ReturnsValid()
+    {
+        var validator = new JsonValidator();
+        var result = validator.Validate(" \n\t { \"a\": 1 } \r\n ");
+
+        Assert.True(result.IsValid);
+    }
+
+    [Fact]
+    public void Validate_InputStartingWithBrace_IsSyntaxError()
+    {
+        var validator = new JsonValidator();
+
+        var result = validator.Validate("{ this is not valid json");
+
+        Assert.False(result.IsValid);
+        Assert.Equal(JsonErrorKind.SyntaxError, result.ErrorKind);
+    }
+
+    [Fact]
+    public void Validate_InputStartingWithBracket_IsSyntaxError()
+    {
+        var validator = new JsonValidator();
+
+        var result = validator.Validate("[ not json");
+
+        Assert.False(result.IsValid);
+        Assert.Equal(JsonErrorKind.SyntaxError, result.ErrorKind);
+    }
+
+    [Fact]
+    public void Validate_PlainText_IsNotJson()
+    {
+        var validator = new JsonValidator();
+
+        var result = validator.Validate("hello world");
+
+        Assert.False(result.IsValid);
+        Assert.Equal(JsonErrorKind.NotJson, result.ErrorKind);
+    }
+
+    [Fact]
+    public void Validate_WhitespaceBeforeBrace_IsSyntaxError()
+    {
+        var validator = new JsonValidator();
+    
+        var result = validator.Validate("   \n\t { bad json }");
+    
+        Assert.False(result.IsValid);
+        Assert.Equal(JsonErrorKind.SyntaxError, result.ErrorKind);
     }
 }
